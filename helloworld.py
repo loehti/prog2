dec3c627ad1cc7537b6dbe0698d6d58ab80a59aa
loehti <83419580+loehti@users.noreply.github.com>
2022-01-15 17:10:45 +0100

    -

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,18 @@
-print("Hello World")
+from flask import Flask
+from flask import render_template
+
+app = Flask("Hello World")
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html', name="Laeticia")
+
+
+@app.route("/test")
+def test():
+    return "Passt!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
