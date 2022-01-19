fb1c33b2097b58f48e5c5263eca684ed238d84cd
loehti <83419580+loehti@users.noreply.github.com>
2022-01-19 23:36:07 +0100

    -

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,30 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask("mypaw")
+mypets = [("cat", "henry", 7, "kibble", "none", "anxiety", "owner"),
+          ("dog", "lucy", 3, "wet food", "cbd", "none", "foster")
+          ]
+
+# @app.route("/mypets", methods=["GET", "POST"])
+#def home():
+    #print(request.form)
+    #mypets.append(
+        #(
+           # request.form.get("animal"),
+            #request.form.get("name"),
+            #float(request.form.get("age")),
+            #request.form.get("food"),
+            #request.form.get("medication"),
+            #request.form.get("special needs"),
+            #request.form.get("status"),
+# ))
+    #return render_template("mypets.html")
+
+
+@app.route("/savedpets")
+def show_savedpets():
+    return render_template("savedpets.html")
 
 
 @app.route("/")
@@ -9,9 +32,9 @@ def hello():
     return render_template('index.html', name="Laeticia")
 
 
-@app.route("/test")
-def test():
-    return "Passt!"
+@app.route("/mypets")
+def mypets():
+    return render_template('mypets.html')
 
 
 @app.route("/")
