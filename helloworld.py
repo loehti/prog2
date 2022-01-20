5c29884c5ae3b07096e66cc4ea174e5f042147af
loehti <83419580+loehti@users.noreply.github.com>
2022-01-20 23:47:43 +0100

    -

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,29 @@
 from flask import Flask
 from flask import render_template, request
-
+import time
 app = Flask("mypaw")
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html', name="Laeticia")
+
+
+@app.route("/mypets")
+def mypets():
+    return render_template('mypets.html')
+
+
+@app.route("/")
+def logo():
+    return render_template('img src="static/img/paw.png"')
+
+
 mypets = [("cat", "henry", 7, "kibble", "none", "anxiety", "owner"),
-          ("dog", "lucy", 3, "wet food", "cbd", "none", "foster")
-          ]
+         ("dog", "lucy", 3, "wet food", "cbd", "none", "foster")
+           ]
+
+# this was supposed to be a list example for the pets
 
 # @app.route("/mypets", methods=["GET", "POST"])
 #def home():
@@ -21,25 +40,29 @@ mypets = [("cat", "henry", 7, "kibble", "none", "anxiety", "owner"),
 # ))
     #return render_template("mypets.html")
 
+# 1) this code was supposed to save the pets (like a database)
 
 @app.route("/savedpets")
 def show_savedpets():
     return render_template("savedpets.html")
 
-
-@app.route("/")
-def hello():
-    return render_template('index.html', name="Laeticia")
+# 2) and then be displayed on /savedpets but it didn't work :(
 
 
-@app.route("/mypets")
-def mypets():
-    return render_template('mypets.html')
 
+# t = int(input("How many hours should the timer be set for?"))
 
-@app.route("/")
-def logo():
-    return render_template('img src="static/img/paw.png"')
+#while t:
+ #   hrs = t // 3600
+#  mins = t // 60
+#   secs = t % 60
+#   timer = '{:02d}:{:02d}:{:02d}'.format(hrs, mins, secs)
+#   print(timer, end="\r")
+#   time.sleep(1)
+#   t -= 1
+#print('Time for medicine!')
+
+#countdown for the next dose of medicine
 
 
 if __name__ == "__main__":
