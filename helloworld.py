f30e239bc05ae5c481b69e106c3868a17d9d8412
loehti <83419580+loehti@users.noreply.github.com>
2022-01-21 15:23:57 +0100

    -

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request
 import time
+
 app = Flask("mypaw")
 
 
@@ -19,26 +20,31 @@ def logo():
     return render_template('img src="static/img/paw.png"')
 
 
-mypets = [("cat", "henry", 7, "kibble", "none", "anxiety", "owner"),
-         ("dog", "lucy", 3, "wet food", "cbd", "none", "foster")
-           ]
+# @app.route("/savedpets")
+# def example():
+#   return render_template('img src="static/img/example.png"')
+
+
+# mypets = [("cat", "henry", 7, "kibble", "none", "anxiety", "owner"),
+#        ("dog", "lucy", 3, "wet food", "cbd", "none", "foster")
+#         ]
 
 # this was supposed to be a list example for the pets
 
 # @app.route("/mypets", methods=["GET", "POST"])
-#def home():
-    #print(request.form)
-    #mypets.append(
-        #(
-           # request.form.get("animal"),
-            #request.form.get("name"),
-            #float(request.form.get("age")),
-            #request.form.get("food"),
-            #request.form.get("medication"),
-            #request.form.get("special needs"),
-            #request.form.get("status"),
+# def home():
+# print(request.form)
+# mypets.append(
+# (
+# request.form.get("animal"),
+# request.form.get("name"),
+# float(request.form.get("age")),
+# request.form.get("food"),
+# request.form.get("medication"),
+# request.form.get("special needs"),
+# request.form.get("status"),
 # ))
-    #return render_template("mypets.html")
+# return render_template("mypets.html")
 
 # 1) this code was supposed to save the pets (like a database)
 
@@ -46,23 +52,27 @@ mypets = [("cat", "henry", 7, "kibble", "none", "anxiety", "owner"),
 def show_savedpets():
     return render_template("savedpets.html")
 
-# 2) and then be displayed on /savedpets but it didn't work :(
 
+# 2) and then be displayed on /savedpets but it didn't work :(
 
 
 # t = int(input("How many hours should the timer be set for?"))
 
-#while t:
- #   hrs = t // 3600
+# while t:
+#   hrs = t // 3600
 #  mins = t // 60
 #   secs = t % 60
 #   timer = '{:02d}:{:02d}:{:02d}'.format(hrs, mins, secs)
 #   print(timer, end="\r")
 #   time.sleep(1)
 #   t -= 1
-#print('Time for medicine!')
+# print('Time for medicine!')
 
-#countdown for the next dose of medicine
+# countdown for the next dose of medicine
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
